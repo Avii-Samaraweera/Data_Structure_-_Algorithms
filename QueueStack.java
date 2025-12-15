a743524829d9979f43172ac88c800d01c7ad8a27
Avii-Samaraweera <sanduniavishka500@gmail.com>
2025-12-15 08:36:11 +0530

    Implement Queue using two stacks

---
+++ QueueStack.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class QueueUsingStack {
+    Stack<Integer> s1 = new Stack<>();
+    Stack<Integer> s2 = new Stack<>();
+
+    // Enqueue
+    void enqueue(int x) {
+        s1.push(x);
+    }
+
+    // Dequeue
+    int dequeue() {
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
+    }
+}

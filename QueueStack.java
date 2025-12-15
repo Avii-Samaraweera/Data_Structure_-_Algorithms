ab958d4ea3ccccefd226b36af754c9746f04ed56
Avii-Samaraweera <sanduniavishka500@gmail.com>
2025-12-15 08:43:38 +0530

    Update QueueStack.java

--- QueueStack.java
+++ QueueStack.java
@@ -1,21 +1,23 @@
 import java.util.Stack;
 
+//use two Stacks to push and pop 
 class QueueUsingStack {
     Stack<Integer> s1 = new Stack<>();
     Stack<Integer> s2 = new Stack<>();
 
-    // Enqueue
+    // Enqueue- push friendly, easy to enqueue
     void enqueue(int x) {
         s1.push(x);
     }
 
-    // Dequeue
-    int dequeue() {
-        if (s2.isEmpty()) {
-            while (!s1.isEmpty()) {
+    // Dequeue- hard to dequeue
+    int dequeue() { 
+        if (s2.isEmpty()) { // check s2 is empty  
+            while (!s1.isEmpty()) { 
                 s2.push(s1.pop());
+            //pop from s1 and push it to s2 until s1 is empty 
             }
         }
-        return s2.pop();
+        return s2.pop(); 
     }
 }

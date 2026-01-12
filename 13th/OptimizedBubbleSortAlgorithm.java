feeded4837d677f85685f2443912fe7b90157c22
Avii-Samaraweera <sanduniavishka500@gmail.com>
2026-01-12 09:12:36 +0530

    Create OptimizedBubbleSortAlgorithm.java

---
+++ 13th/OptimizedBubbleSortAlgorithm.java
@@ -0,0 +1,35 @@
+public class OptimizedBubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped)
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted Array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
